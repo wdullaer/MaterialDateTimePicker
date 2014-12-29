050a9945c3139ccde30ec85924df8a5e1b83ba5d
wdullaer <wouter.dullaert@gmail.com>
2014-12-29 18:53:09 +0100

    Initial commit

---
+++ library/src/main/java/com/wdullaer/materialdatetimepicker/date/DatePickerController.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wdullaer.materialdatetimepicker.date;
+
+import java.util.Calendar;
+
+/**
+ * Controller class to communicate among the various components of the date picker dialog.
+ */
+public interface DatePickerController {
+
+    void onYearSelected(int year);
+
+    void onDayOfMonthSelected(int year, int month, int day);
+
+    void registerOnDateChangedListener(DatePickerDialog.OnDateChangedListener listener);
+
+    void unregisterOnDateChangedListener(DatePickerDialog.OnDateChangedListener listener);
+
+    MonthAdapter.CalendarDay getSelectedDay();
+
+    int getFirstDayOfWeek();
+
+    int getMinYear();
+
+    int getMaxYear();
+
+    Calendar getMinDate();
+
+    Calendar getMaxDate();
+
+    void tryVibrate();
+}
