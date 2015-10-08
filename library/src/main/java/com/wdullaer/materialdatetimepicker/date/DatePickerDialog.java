d3f887b6b27cf4bf60a1775d7337e0fb54aff808
Wouter Dullaert <wouter.dullaert@gmail.com>
2015-10-08 07:24:17 +0200

    Fixes #95 Always reinitialise the mCurrentView in the Dialog's onCreate to ensure all Views are initialised correctly

--- library/src/main/java/com/wdullaer/materialdatetimepicker/date/DatePickerDialog.java
+++ library/src/main/java/com/wdullaer/materialdatetimepicker/date/DatePickerDialog.java
@@ -184,6 +184,7 @@ public class DatePickerDialog extends DialogFragment implements
         final Activity activity = getActivity();
         activity.getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        mCurrentView = UNINITIALIZED;
         if (savedInstanceState != null) {
             mCalendar.set(Calendar.YEAR, savedInstanceState.getInt(KEY_SELECTED_YEAR));
             mCalendar.set(Calendar.MONTH, savedInstanceState.getInt(KEY_SELECTED_MONTH));
